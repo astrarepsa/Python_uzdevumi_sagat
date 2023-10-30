b8f6bb4efaf9387cfdd37209a7192c6a178c740f
astrarepsa <97939332+astrarepsa@users.noreply.github.com>
2023-10-30 10:30:52 +0200

    Update 7uzd.py

--- 7uzd.py
+++ 7uzd.py
@@ -1,7 +1,7 @@
 """
 Papildināt programmu ar ciklu, kurā sarakstā esošiem uzvārdiem tiktu 
 pievienots klāt doktora nosaukums - Dr.
-"""
+"""""
 
 
 saraksts1=["Kalniņš", "Opmanis", "Vēzis", "Almane"]
